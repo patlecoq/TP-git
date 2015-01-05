776964c791f4d950f34f0ac102ab3dc320100e03
Patrice LE COQ <patlecoq@hotmail.com>
2015-01-05 15:09:56 +0100

    add partner name

--- application.bat
+++ application.bat
@@ -1,4 +1,4 @@
 ### application.bat - 2015 - Patrice LE COQ ###
 echo "Copyright CTV - 2015"
 echo "Build : 10:00"
-echo "version 1.1"
+echo "version 1.2"
---
+++ test/test_app.bat
@@ -0,0 +1,1 @@
+### test_bat.bat - 2015 - Patrice LE COQ ###
