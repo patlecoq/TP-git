a00d29bc13f2b54b5f04488a341ce36a8ba08f24
Nicolas Roudot <nicolas.roudot@imie-rennes.fr>
2015-01-06 09:33:24 +0100

    update header for new member

--- application.bat
+++ application.bat
@@ -1,4 +1,4 @@
-### application.bat - 2015 - Patrice LE COQ ###
+### application.bat - 2015 - Patrice LE COQ - Nicolas ROUDOT ###
 echo "Copyright CTV - 2015"
 echo "Build : 10:00"
 echo "version 1.2"
